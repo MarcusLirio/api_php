1eff4495b1d26851041b33a0916b1c80e23323db
eletriza <marcus.lirio1@gmail.com>
2023-04-23 02:54:16 -0300

    .

--- config.php
+++ config.php
@@ -1,10 +1,5 @@
 <?php 
     //Postgres
 
-/// DB on here
-// const DBDRIVE = "pgsql";
-// const DBHOST = "localhost";
-// const DBNAME = "eletrizadb";
-// const DBUSER = "eletriza";
-// const DBPASS = "123456";
+// / DB on here
 ?>
